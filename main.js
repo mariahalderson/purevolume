a3291d40edd127e5156ace01c7345c646400a476
Trevor Van Rys <trevorvanrys@Trevors-MacBook-Pro-2.local>
2018-09-08 11:23:39 -0400

    another solution"

--- main.js
+++ main.js
@@ -1,53 +1,32 @@
-var heightText = document.querySelector("p"),
-    knob = document.querySelector("#outerring"),
-    last_pos = 0,
-    ticking = false;
+(() => {
+  let heightText = document.querySelector("p"),
+      knob = document.querySelector("#outerring"),
+      last_pos = 0;
 
+  function scrollDir() {
+    let current_pos = window.scrollY; // get the scroll position (can do this a couple of different ways)
 
+    // ternary statement (shorthand if / else) - if the condition is true, animDir is what's to the left of the colon. if it's false, then it's whatever is to the right of the colon
+    let animDir = (current_pos > last_pos) ? "knobSpin" : "knobSpinRev";
 
-//find scroll height
-function setFrame() {
-  //set last_pos to scroll #
-	last_pos = window.scrollY;
+    // reset last position for the next iteration of the function
+    last_pos = current_pos;
+    heightText.textContent = last_pos;
 
-	if (!ticking) {
+    // run the animation
+    knobAnimation(animDir);
+  }
+
+  function knobAnimation(direction) {
+    //remove animations
+    knob.classList.remove("knobSpin");
+    knob.classList.remove("knobSpinRev");
     
-		window.requestAnimationFrame(function() {
-      //set p tag to show scroll rounded to nearest whole nubmer
-      heightText.innerHTML = Math.round(last_pos);
-      knobAnimation();
-
-			ticking = false;
-
-		});
-
-		ticking = true;
-	}
-
-}
+    knob.classList.add(direction);
+  }
 
 
+  //run setframe on window scroll
+  window.addEventListener('scroll', scrollDir, false);
+})();
 
-
-
-function knobAnimation(){
-  //remove animations
-  knob.classList.remove("knobSpin");
-  knob.classList.remove("knobSpinRev");
-  
-  if(last_pos ++){
-    knob.classList.add("knobSpin");
-    // knob.style.animationPlayState = "running";
-    // knob.style.animationDirection = "normal";
-  }else if(last_pos --){
-    knob.classList.add("knobSpinRev");
-    // knob.style.animationPlayState = "running";
-    // knob.style.animationDirection = "reverse";
-  //}else{
-    // knob.style.animationPlayState = "paused";
-  }  
-}
-
-
-//run setframe on window scroll
-window.addEventListener('scroll', setFrame, false);
