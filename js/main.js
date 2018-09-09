b67ef90406983453373106d80e0091a07b9cc5a4
Mariah Alderson <mariah.alderson@gmail.com>
2018-09-08 21:11:24 -0400

    home page styling

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var hamburger = document.querySelector("#hamburgermenu"),
+	topbar = document.querySelector(".bar1"),
+	midbar = document.querySelector(".bar2"),
+	lowbar = document.querySelector(".bar3"),
+	nav = document.querySelector("#navmenu");
+
+
+	 hamburger.addEventListener('click', function (){
+	 	//toggle rotating transitions when open/closing mobile menu
+	 	 topbar.classList.toggle("change-bar1");
+	 	 midbar.classList.toggle("change-bar2");
+	 	 lowbar.classList.toggle("change-bar3");
+
+	 	 //nav.style.display = "block";
+	 	 nav.classList.toggle("change-nav");
+	 	 nav.classList.toggle("before-nav");
+
+	});
