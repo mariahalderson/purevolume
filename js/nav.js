d44dde36fa1f1bc3ea585037c872b5574af29b95
Mariah Alderson <mariah.alderson@gmail.com>
2018-09-11 11:27:26 -0400

    nav bugs fixed

--- js/nav.js
+++ js/nav.js
@@ -1,52 +1,96 @@
 var navblock = document.querySelector("#navhighlight"),
 	items = Array.from(document.querySelectorAll("#navlist li a")),
-	last_pos = 0,
 	popheight = document.querySelector("#popculture").offsetHeight,
 	mheight = document.querySelector("#music").offsetHeight,
 	eheight = document.querySelector("#entertainment").offsetHeight,
 	heroheight = document.querySelector("#hero").offsetHeight;
 
-console.log(items[2].innerHTML);
+
+
+
+function start(){
+	resettext();
+
+		navblock.style.width = "2px";
+		navblock.style.transform = "translateX(0px)";
+}
+
+
+
+
+function popculture(){
+	resettext();
+
+		navblock.style.width = "132px";
+		navblock.style.transform = "translateX(150px)";	
+
+		items[1].style.color = "#fff";
+}
+
+
+
+
+function entertainment(){
+	resettext();
+
+		navblock.style.width = "164px";
+		navblock.style.transform = "translateX(352px)";
+
+		items[2].style.color = "#fff";
+}
+
+
+
+
+function music(){
+	resettext();
+
+		navblock.style.width = "67px";
+		navblock.style.transform = "translateX(455px)";
+
+		items[3].style.color = "#fff";
+}
+
+
+
+
+function resettext(){
+
+	items.forEach(item=>item.style.color = "#283744");
+
+}
+
+
+
 	
 	function moveblock(){
 
 		let current_pos = window.scrollY;
 		console.log(current_pos);
 
-	if(current_pos>0){
-		resettext();
+	if(current_pos< heroheight){
 
-		navblock.style.width = "2px";
-		navblock.style.transform = "translateX(0px)";
+		start();
 
-	
 	}if(current_pos > heroheight){
-		resettext();
-
-		navblock.style.width = "132px";
-		navblock.style.transform = "translateX(150px)";	
-
-		items[1].style.color = "#fff";
+		
+		popculture();
 
 	}if(current_pos > heroheight + popheight){
-		resettext();
+		
+		entertainment();
 
-		navblock.style.width = "164px";
-		navblock.style.transform = "translateX(352px)";
-
-		items[2].style.color = "#fff";
 	}if(current_pos > heroheight + popheight + eheight){
-		resettext();
-
-		navblock.style.width = "67px";
-		navblock.style.transform = "translateX(455px)";
-
-		items[3].style.color = "#fff";
+		
+		music();
 	}
 }
 
-function resettext(){
-	items.forEach(item=>item.style.color = "#283744");
-}
 
 window.addEventListener('scroll', moveblock, false);
+
+items[1].addEventListener('mouseenter', popculture);
+items[2].addEventListener('mouseenter', entertainment);
+items[3].addEventListener('mouseenter', music);
+
+items.forEach(item=>item.addEventListener('mouseout', moveblock));
